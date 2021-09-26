b24554e3ae503b843397e3a3cb2450ddf5e6e5e2
Caroline Tan <carolinectan3@gmail.com>
2021-09-26 14:05:28 -0400

    Update forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -3,11 +3,13 @@ class ForecastFacade
     json = ForecastService.get_forecast(location)
 
     current_forecast = CurrentForecast.new(json[:current])
-    daily_forecast = json[:daily][0..4].each do |daily|
+    daily_forecast = json[:daily][0..4].map do |daily|
       DailyForecast.new(daily)
     end
-    hourly_forecast = json[:hourly][0..7].each do |hourly|
+    hourly_forecast = json[:hourly][0..7].map do |hourly|
       HourlyForecast.new(hourly)
     end
+
+    weather = Weather.new(current_forecast, daily_forecast, hourly_forecast)
   end
 end
