d40f0067429b7f41ce35c60c06a11145f9e8d334
Caroline Tan <carolinectan3@gmail.com>
2021-09-26 18:51:57 -0400

    Start background serializer; wip

---
+++ app/serializers/api/v1/background_serializer.rb
@@ -0,0 +1,22 @@
+class Api::V1::BackgroundSerializer
+  def self.get_background(background)
+    {
+      "data": {
+    #     "type": "image",
+    #     "id": null,
+    #     "attributes": {
+    #       "image": {
+    #         "location": "denver,co",
+    #         "image_url": "https://pixabay.com/get/54e6d4444f50a814f1dc8460962930761c38d6ed534c704c7c2878dd954dc451_640.jpg",
+    #         "credit": {
+    #           "source": "pixabay.com",
+    #           "author": "quinntheislander",
+    #           "logo": "https://pixabay.com/static/img/logo_square.png"
+    #         }
+    #       }
+    #     }
+    #   }
+    # }
+
+  end
+end
