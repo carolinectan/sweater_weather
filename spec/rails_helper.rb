2016eea96795b2bada7619f3ccaa4ae0c2e80887
Caroline Tan <carolinectan3@gmail.com>
2021-09-25 20:21:26 -0400

    Add hide map api key to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,7 +82,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.filter_sensitive_data('DONT_SHARE_MY_SECRET_KEY') { ENV['<api_key>'] }
+  config.filter_sensitive_data('DONT_SHARE_MY_SECRET_KEY') { ENV['open_weather_api_key'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata! # look this up
 end
