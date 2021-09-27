bd3d3e56fc7e158ae3e2ffa64c0ccce080d0ae17
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 13:22:54 -0400

    Refactor location service: conn is only protocol

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,6 +1,6 @@
 class LocationService
   def self.find_lat_long(location)
-    response = conn('https://www.mapquestapi.com/geocoding/v1/address').get do |req|
+    response = conn.get('/geocoding/v1/address') do |req|
       req.params['key'] = ENV['mapquest_api_key']
       req.params['inFormat'] = 'kvp'
       req.params['outFormat'] = 'json'
@@ -10,7 +10,7 @@ class LocationService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.conn(endpoint)
-    Faraday.new(endpoint)
+  def self.conn
+    Faraday.new('https://www.mapquestapi.com')
   end
 end
