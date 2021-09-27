54d5b5ea3aa2420de54ac7a095308050cd7b06fb
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 13:48:47 -0400

    Add more tests to location facade spec

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe LocationFacade do
     describe '.find_lat_long' do
       it 'returns a location object', :vcr do
         location_query = 'denver,co'
-
         location = LocationFacade.find_lat_long(location_query)
 
         expect(location).to_not be_an Array
         expect(location).to be_a Location
+        expect(location.latitude).to eq(39.738453)
+        expect(location.longitude).to eq(-104.984853)
       end
     end
   end
