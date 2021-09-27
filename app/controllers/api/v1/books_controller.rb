44e8d301369f22023d1b0aaf678d2a07ff6ece49
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 12:12:06 -0400

    Add location and forecast facade and Book Serializer to index

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    book = BooksFacade.find_books(params[:location], params[:quantity])
+    books = BooksFacade.find_books(params[:location], params[:quantity])
+    location = LocationFacade.find_lat_long(params[:location])
+    forecast = ForecastFacade.get_forecast(location)
+
+    render json: BookSerializer.find_books(params[:location], params[:quantity], books, forecast)
   end
 end
