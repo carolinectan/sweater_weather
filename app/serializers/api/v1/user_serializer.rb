5e8b77c7fbd86270cb7bb8be5e8d3a7aceb91a9c
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 18:55:43 -0400

    Add namespacing and convert id to string

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,11 +1,11 @@
-class UserSerializer
+class Api::V1::UserSerializer
   include FastJsonapi::ObjectSerializer
 
   def self.generate_api_key(new_user)
     {
       "data": {
         "type": "users",
-        "id": new_user.id,
+        "id": new_user.id.to_s,
         "attributes": {
           "email": new_user.email,
           "api_key": "jgn983hy48thw9begh98h4539h4"
