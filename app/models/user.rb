7654edef82e693aaba00a7f69c5af4fe6e4515aa
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 18:56:37 -0400

    Add user validations for empty and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-  validates_presence_of :password, require: true, confirmation: true
+  validates :email, presence: true, uniqueness: true, empty: false
+  validates_presence_of :password, require: true, confirmation: true, empty: false
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should_not allow_values('').for(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
+    it { should_not allow_values('').for(:password) }
+    it { should have_secure_password }
   end
 end
