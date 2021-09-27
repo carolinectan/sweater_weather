87a12af32747f5319b9661f65cdea8746d1e46ee
Caroline Tan <carolinectan3@gmail.com>
2021-09-27 13:16:29 -0400

    Change quantity in book facade spec

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe BookFacade do
     describe '.find_books' do
       it 'returns an array of books', :vcr do
         location = 'denver,co'
-        quantity = '5'
+        quantity = '7'
         books = BookFacade.find_books(location, quantity)
 
         expect(books).to be_an Array
         expect(books.first).to be_a Book
-        expect(books.size).to eq(5)
+        expect(books.size).to eq(7)
       end
     end
   end
