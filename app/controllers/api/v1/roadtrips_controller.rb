27b5e8c4360c4cd554ac3a2b26191feb0fa61dbd
Caroline Tan <carolinectan3@gmail.com>
2021-09-28 15:48:52 -0400

    Add roadtrip create (wip), bad credentials, and strong params

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -1,5 +1,27 @@
 class Api::V1::RoadtripsController < ApplicationController
   def create
-    
+    user = User.find_by(api_key: roadtrip_params[:api_key])
+
+    if user.nil? || params[:api_key].nil?
+      bad_credentials_error
+    end
+
+    roadtrip = RoadtripFacade.get_roadtrip(roadtrip_params[:origin], roadtrip_params[:destination])
+    require "pry"; binding.pry
+    render json: RoadtripSerializer.get_roadtrip(roadtrip)
+  end
+
+  def bad_credentials_error
+    render json: {
+      message: 'Your request could not be completed.',
+      errors: ['Bad credentials.']
+    },
+           status: :unauthorized # 401
+  end
+
+  private
+
+  def roadtrip_params
+    params.require(:roadtrip).permit(:origin, :destination, :api_key)
   end
 end
