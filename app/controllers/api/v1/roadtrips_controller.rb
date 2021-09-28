174b409aaf40be69cd4affbae7a5e3350b06e27a
Caroline Tan <carolinectan3@gmail.com>
2021-09-28 18:21:46 -0400

    Refactor conditional; add impossible route method

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -4,11 +4,12 @@ class Api::V1::RoadtripsController < ApplicationController
 
     if user.nil? || params[:api_key].nil?
       bad_credentials_error
+    elsif RoadtripFacade.get_roadtrip(roadtrip_params[:origin], roadtrip_params[:destination]) == 402
+      render json: impossible_route
     else
       roadtrip = RoadtripFacade.get_roadtrip(roadtrip_params[:origin], roadtrip_params[:destination])
       destination = LocationFacade.find_lat_long(roadtrip_params[:destination])
       destination_weather = ForecastFacade.get_hourly_only(destination)
-
       render json: RoadtripSerializer.get_roadtrip(roadtrip, destination_weather)
     end
   end
@@ -19,6 +20,25 @@ class Api::V1::RoadtripsController < ApplicationController
       errors: ['Bad credentials.']
     },
            status: :unauthorized # 401
+  end
+
+  def impossible_route
+    null = nil
+    {
+      data: {
+        id: null,
+        type: 'roadtrip',
+        attributes: {
+          start_city: roadtrip_params[:origin],
+          end_city: roadtrip_params[:destination],
+          travel_time: 'Impossible route',
+          weather_at_eta: {
+            temperature: null,
+            conditions: null
+          }
+        }
+      }
+    }
   end
 
   private
