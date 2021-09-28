ba4dbd489888ba69d937a6541eb47988cf71d303
Caroline Tan <carolinectan3@gmail.com>
2021-09-28 13:40:39 -0400

    Comment out faker and factory bot

--- Gemfile
+++ Gemfile
@@ -34,8 +34,8 @@ gem 'fast_jsonapi'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'factory_bot_rails'
-  gem 'faker'
+  # gem 'factory_bot_rails'
+  # gem 'faker'
   gem 'figaro'
   gem 'pry'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'faker'
+# require 'faker'
 require 'simplecov' # look where simplecov should live
 SimpleCov.start do
   add_filter 'spec/'
@@ -69,7 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include FactoryBot::Syntax::Methods
+  # config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
