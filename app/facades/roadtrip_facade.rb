4ebcea241be56898972478e7730ebdc49daaa239
Caroline Tan <carolinectan3@gmail.com>
2021-09-28 18:19:51 -0400

    Add conditional to roadtrip facade for impossible route

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -2,6 +2,10 @@ class RoadtripFacade
   def self.get_roadtrip(origin, destination)
     json = RoadtripService.get_roadtrip(origin, destination)
 
-    Roadtrip.new(json, origin, destination)
+    if !json[:route][:formattedTime].nil?
+      Roadtrip.new(json, origin, destination)
+    else
+      json[:info][:statuscode]
+    end
   end
 end
