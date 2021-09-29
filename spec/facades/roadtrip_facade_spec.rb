507c3068c19a4aec7d6d3aef71b1a00fe44daa77
Caroline Tan <carolinectan3@gmail.com>
2021-09-29 01:44:31 -0400

    Add roadtrip facade spec

---
+++ spec/facades/roadtrip_facade_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe RoadtripFacade do
+  describe 'class methods' do
+    describe '.find_lat_long' do
+      context 'happy path' do
+        it 'returns a roadtrip object', :vcr do
+          origin = 'denver,co'
+          destination = 'pueblo,co'
+
+          roadtrip = RoadtripFacade.get_roadtrip(origin, destination)
+
+          expect(roadtrip).to be_a Roadtrip
+        end
+      end
+
+      context 'sad path' do
+        it 'returns an status code', :vcr do
+          origin = 'denver,co'
+          destination = 'london,uk'
+
+          roadtrip = RoadtripFacade.get_roadtrip(origin, destination)
+
+          expect(roadtrip).to eq(402)
+        end
+      end
+    end
+  end
+end
